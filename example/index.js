008eaaab63c05524b4d465e3c831695279b32084
YoneyY <1216368461@qq.com>
2022-02-19 20:28:21 +0800

    feat: Modify module import and export methods

--- example/index.js
+++ example/index.js
@@ -1,11 +1,36 @@
+const fs = require('fs');
 const strdm = require('strdm');
+const { WxSign } = require('wsign');
 
-console.log(strdm());          // dZUYsl
-console.log(strdm(32));        // ilq4i3rXe70rz3j4hrpjMKI3jqCDpr0q
-console.log(strdm(32, true));  // 8AMl$2+8jC25u}>DXjjo9p]n.,vWhzVU
-console.log(strdm(32, { numbers: false }));       // iwhfOOHlEFNIbuTGHseCyCYnppCXoraZ
-console.log(strdm(32, { strings: false }));       // 56124247096321521961829045800161
-console.log(strdm(32, { strings: 'ABCDEFG' }));   // 8EE9B8GFF79CF28AADA9A2G439242211
-console.log(strdm(32, { symbols: true }));        // HccU8vM)1,TVHi$=J,::V^wiMxY(tG-j
-console.log(strdm(32, { symbols: true, numbers: false, strings: false }));    // ,|+;%]@;,_+#(%.~;[@!{}-<~?@=;;??
-console.log(strdm(32, { symbols: ':;', numbers: false, strings: false }));    // :;;;::;;:::::;::::;:;::;:;::;;;:
+console.log(WxSign)
+
+const wxsign = new WxSign({
+  key: 'ilq4i3rXe70rz3j4hrpjMKI3jqCDpr0q',
+  agent: {
+    key: fs.readFileSync('/srv/ssl/wxsign/apiclient_key.pem'),
+    cert: fs.readFileSync('/srv/ssl/wxsign/apiclient_cert.pem'),
+  }
+});
+
+const mchid = '1111111';
+const nonce_str = strdm(32);
+const partner_trade_no = wxsign.generateOrder(32);
+const openid = 'ilq4i3rXe70rz3j4hrpjMKI3jqCDpr0q';
+
+wxsign.setParams({
+  amount: 100,                 // 付款金额 单位：分
+  check_name: 'NO_CHECK',      // NO_CHECK 不校验真实姓名 FORCE_CHECK 强校验真实姓名
+  desc: '活动奖金',             // 描述 / 付款备注
+  mch_appid,                   // 商户 appID
+  mchid,                       // 商户ID
+  nonce_str,                   // 随机字符串
+  openid,                      // openID
+  partner_trade_no,            // 订单号
+});
+
+wxsign.setData('workwx_sign', wpay.generateSign());
+wxsign.setData('agentid', 3010046);
+wxsign.setData('remark', 'RedBag');
+wxsign.setData('wishing', 'RedBag');
+
+console.log(wxsign.generateSignXML('sign'))
